b582e88fec7df9ed86a6b4b13d8198839b1382c3
maxmaxamx <maximnovitskiy274@gmail.com>
2025-06-08 14:34:39 +0300

    web development project

---
+++ js/blog.js
@@ -0,0 +1,46 @@
+let blogs = [
+  {
+    image: './images/seo.jpg',
+    head: 'SEO Quis Vestibulum',
+    date: '14.05.25',
+    category:'development',
+    tieser:'Dolore magna aliqua. Ut enim ad minim veniam, quis nos-<br>trud exer ullamco laboris nisi ut aliquip ex ea com-<br>modo consequat...'
+  },
+  {
+    image: './images/work-space.jpg',
+    head: 'SEO Quis Vestibulum',
+    date: '14.05.25',
+    category:'development',
+    tieser:'Dolore magna aliqua. Ut enim ad minim veniam, quis nos-<br>trud exer ullamco laboris nisi ut aliquip ex ea com-<br>modo consequat...'
+  },
+  {
+    image: './images/work-space-2.jpg',
+    head: 'SEO Quis Vestibulum',
+    date: '14.05.25',
+    category:'development',
+    tieser:'Dolore magna aliqua. Ut enim ad minim veniam, quis nos-<br>trud exer ullamco laboris nisi ut aliquip ex ea com-<br>modo consequat...'
+  },
+  {
+    image: './images/seo.jpg',
+    head: 'SEO Quis Vestibulum',
+    date: '14.05.25',
+    category:'development',
+    tieser:'Dolore magna aliqua. Ut enim ad minim veniam, quis nos-<br>trud exer ullamco laboris nisi ut aliquip ex ea com-<br>modo consequat...'
+  },
+  {
+    image: './images/work-space.jpg',
+    head: 'SEO Quis Vestibulum',
+    date: '14.05.25',
+    category:'development',
+    tieser:'Dolore magna aliqua. Ut enim ad minim veniam, quis nos-<br>trud exer ullamco laboris nisi ut aliquip ex ea com-<br>modo consequat...'
+  },
+  {
+    image: './images/work-space-2.jpg',
+    head: 'SEO Quis Vestibulum',
+    date: '14.05.25',
+    category:'development',
+    tieser:'Dolore magna aliqua. Ut enim ad minim veniam, quis nos-<br>trud exer ullamco laboris nisi ut aliquip ex ea com-<br>modo consequat...'
+  }
+];
+
+
--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,48 @@
+$(document).ready(function(){
+  AOS.init();
+  
+   getBlogs(blogsCount);
+  $(".portfolio-button").on("click", viewMore);
+});
+
+let blogsCount = 3;
+const step = 1;
+
+function getBlogs(count){
+  let items = blogs.slice(0, count);
+
+  $(".blogs").html('');
+  items.forEach(function(blog){
+    $(".blogs").append(`
+      <div class="blogContainer">
+        <div class="blog-item">
+            <img src="${blog.image}" alt="">
+        </div>
+        <div class="blog-item">
+            <a href="#">${blog.head}</a>
+            <div class="date"><img src="./images/calendar.png" alt=""><p>${blog.date}</p><img src="./images/paper.png" alt="" class="marg"><p>${blog.category}</p></div>
+            <p>${blog.tieser}</p>
+        </div>
+      </div>
+    `);
+  });
+}
+
+function viewMore(){
+  blogsCount += step;
+  if(blogsCount >= blogs.length){
+    blogsCount = blogs.length;
+    $(".portfolio-button").hide();
+  }
+  getBlogs(blogsCount);
+}
+
+$(document).ready(function(){
+  getBlogs(blogsCount);
+  $(".portfolio-button").on("click", viewMore);
+});
+
+
 function toggleSearch(event)
 {
     event.stopPropagation();
@@ -11,3 +56,33 @@ document.body.onclick = function(e) {
     let search = document.getElementById("search-box");
     search.classList.remove("active");
 }
+
+const divs = document.getElementsByClassName("service");
+const ser = document.getElementsByClassName("ser");
+const img = document.getElementsByClassName("phone");
+const but = document.getElementsByClassName("more");
+const h = document.getElementsByClassName("hed");
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('mouseover', (event) => {
+    divs[i].style.backgroundColor = 'var(--color-green)';
+    ser[i].style.color = 'var(--color-white)';
+    img[i].src = './images/smartphone_light.png';
+    but[i].style.color = 'var(--color-white)';
+    but[i].style.border = '2px solid var(--color-white)';
+    h[i].style.color = 'var(--color-white)';
+  });
+
+  divs[i].addEventListener('mouseout', (event) => {
+    divs[i].style.backgroundColor = 'var(--color-white)';
+    ser[i].style.color = 'var(--color-black)';
+    img[i].src = './images/smartphone_dark.png'
+    but[i].style.color = 'var(--color-black)';
+    but[i].style.border = '2px solid varvar(--color-black)';
+    but[i].style.border = '2px solid var(--color-black)';
+    h[i].style.color = 'var(--color-black)';
+  });
+}
+
+
+
